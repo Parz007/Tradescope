03484f5648b25dea4db43c2607aa99cc50e5f91d
Parz007 <pbui29399@gmail.com>
2026-06-15 17:30:14 +1000

    Refactor API server import to lazy loading

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-import app from "../artifacts/api-server/dist/vercel.mjs";
-export default app;
+let _app;
+
+module.exports = async (req, res) => {
+  if (!_app) {
+    const mod = await import("../artifacts/api-server/dist/vercel.mjs");
+    _app = mod.default;
+  }
+  _app(req, res);
+};
